8e4a759e528d889772c8733eef7a8d4505d1825f
jmartinezchaine <jmartinezchaine@gmail.com>
2012-10-09 13:00:54 -0200

    Ordena las picking por Origen y Nombre

--- openerp/addons/stock/stock.py
+++ openerp/addons/stock/stock.py
@@ -537,6 +537,7 @@ stock_tracking()
 class stock_picking(osv.osv):
     _name = "stock.picking"
     _description = "Picking List"
+    _order = 'origin, name'
 
     def _set_maximum_date(self, cr, uid, ids, name, value, arg, context=None):
         """ Calculates planned date if it is greater than 'value'.
